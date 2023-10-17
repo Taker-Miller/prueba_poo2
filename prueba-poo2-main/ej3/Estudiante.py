14a9e2b68b5bb022f000271dbda5832eec4e665b
Takerin <96486976+Taker-Miller@users.noreply.github.com>
2023-10-17 16:35:58 -0300

    Estudiante.py

--- prueba-poo2-main/ej3/Estudiante.py
+++ prueba-poo2-main/ej3/Estudiante.py
@@ -5,19 +5,20 @@ class Estudiante:
         self.__notas = notas
 
     def get_nombre(self):
-        return self.__nombre
+        return self.__nombre #devuelve el valor actual del atributo nombre.
 
-    def set_nombre(self, nombre):
+    def set_nombre(self, nombre): #permite cambiar el valor del atributo nombre
         self.__nombre = nombre
 
-    def get_edad(self):
-        return self.__edad
+    def get_edad(self): 
+        return self.__edad #devuelve el valor actual del atributo edad.
 
-    def set_edad(self, edad):
+    def set_edad(self, edad): #permite cambiar el valor del atributo edad
         self.__edad = edad
 
     def get_promedio(self):
-        return sum(self.__notas) / len(self.__notas)
+        return sum(self.__notas) / len(self.__notas) #obtiene el promedio mediante la suma de las notas y luego se divide por la cantidad de notas
 
     def agregar_nota(self, nota):
         self.__notas.append(nota)
+        #agrega notas a la lista de notas de la instancia de la clase en la que se utiliza.
