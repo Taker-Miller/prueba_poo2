901be6015338399cd8e9ad86c0269d13b968ed24
Takerin <96486976+Taker-Miller@users.noreply.github.com>
2023-10-17 16:38:36 -0300

    Empleado.py

--- prueba-poo2-main/ej2/Empleado.py
+++ prueba-poo2-main/ej2/Empleado.py
@@ -1,25 +1,31 @@
-from Empleado import Empleado
-class Gerente(Empleado):
-    def __init__(self, nombre, hrs_trabajadas, salario_por_hr, bono):
-        super().__init__(nombre, hrs_trabajadas, salario_por_hr)
-        self.__bono = bono
+class Empleado:
+    def __init__(self, nombre, hrs_trabajadas, salario_por_hr):
+        self.__nombre = nombre
+        self.__hrs_trabajadas = hrs_trabajadas
+        self.__salario_por_hr = salario_por_hr
 
-    def get_bono(self):
-        return self.__bono #devuelve el valor actual del atributo bono.
-    def set_bono(self, bono):#permite cambiar el valor del atributo bono
-        self.__bono = bono
+    def get_nombre(self):
+        return self.__nombre #devuelve el valor actual del atributo nombre.
+    def set_nombre(self, nombre): #permite cambiar el valor del atributo nombre
+        self.__nombre = nombre
+
+    def get_hrs_trabajadas(self):
+        return self.__hrs_trabajadas  #devuelve el valor actual del atributo hrs_trabajadas.
+    def set_hrs_trabajadas(self, hrs_trabajadas): #permite cambiar el valor del atributo hrs_trabajadas
+        self.__hrs_trabajadas = hrs_trabajadas
+
+    def get_salario_por_hr(self):
+        return self.__salario_por_hr  #devuelve el valor actual del atributo salario_por_hr.
+    def set_salario_por_hr(self, salario_por_hr): #permite cambiar el valor del atributo salario_por_hr
+        self.__salario_por_hr = salario_por_hr
 
     def calcular_salario(self):
-        return super().calcular_salario() + self.__bono #calcula el salario multiplicando salario por hr por hrs trabajadas mas el bono si es que recibe
+        return self.__salario_por_hr * self.__hrs_trabajadas #calcula el salario multiplicando salario por hr por hrs trabajadas
 
     def ver_informacion(self):
         print(f"Nombre: {self.get_nombre()}")
         print(f"Horas trabajadas: {self.get_hrs_trabajadas()}")
         print(f"Salario por hora: {self.get_salario_por_hr()}")
-        print(f"Bono: {self.get_bono()}")
         print(f"Salario total: {self.calcular_salario()}")
-         #muestra la informacion del gerente
+        #muestra la informacion del empleado
 
-
-
-       
