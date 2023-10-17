6befb1cea4ebc3d409e8b07bc8573c674b181d31
Takerin <96486976+Taker-Miller@users.noreply.github.com>
2023-10-17 16:36:15 -0300

    Escuela.py

--- prueba-poo2-main/ej3/Escuela.py
+++ prueba-poo2-main/ej3/Escuela.py
@@ -4,8 +4,9 @@ class Escuela:
         self.__lista_estudiantes = []
     
     def agregar_estudiante(self, estudiante: Estudiante):
-        self.__lista_estudiantes.append(estudiante)
+        self.__lista_estudiantes.append(estudiante) #permite agregar estudiantes a la lista de estudiantes de la instancia de la clase en la que se utiliza.
                 
     def listar_estudiantes(self):
         for estudiante in self.__lista_estudiantes:
             print(f"Nombre: {estudiante.get_nombre()}, Edad: {estudiante.get_edad()}, Promedio: {estudiante.get_promedio()}")
+    #lista y muestra información detallada de cada estudiante, incluyendo su nombre, edad y promedio
